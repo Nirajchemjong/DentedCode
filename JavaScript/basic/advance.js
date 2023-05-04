25c8c053d94eb471b16f2109a01202be88b3ead8
Nirajchemjong <nirajlimbu7@gmail.com>
2023-05-04 19:37:47 +1000

    learning more about javaScript object and array

---
+++ JavaScript/basic/advance.js
@@ -0,0 +1,10 @@
+// alert("hello");
+
+//Object-oriented Programming (OOP)
+
+// Object => properties , methods
+
+const nirajBio = () => {
+  return " this person name is niraj and lives in ACT ";
+};
+console.log(nirajBio(0));
